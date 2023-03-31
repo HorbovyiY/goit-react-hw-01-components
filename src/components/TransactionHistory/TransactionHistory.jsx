cdcec7cb215d82cc631027ff128e7adf82da377f
HorbovyiY <yhorbovyi@gmail.com>
2023-03-31 21:37:44 +0300

    додає transactionhistory

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,24 @@
+import {History } from "./TransactionHistory.styled";
+
+export const TransactionHistory = ({ items }) => {
+    return (
+        <History>
+  <thead>
+    <tr>
+      <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </thead>
+            <tbody>
+                {items.map(({id, type, amount, currency}) =>    
+                <tr key={id}>
+                    <td>{type}</td>
+                    <td>{amount}</td>
+                    <td>{currency}</td>
+                </tr>
+                )}
+  </tbody>
+</History>
+    )
+};
